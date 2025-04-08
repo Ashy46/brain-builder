1529d8ee4d55d2ef92a7d58a6573a09e07fdae2b
Alexandros Lekkas <alexandros.lekkas@outlook.com>
2025-04-07 20:54:52 -0500

    feat: Implement state management for nodes and edges in Graph component to enable dynamic updates and enhance interactivity

--- src/app/dashboard/graphs/[id]/graph.tsx
+++ src/app/dashboard/graphs/[id]/graph.tsx
@@ -1,9 +1,24 @@
-import { ReactFlow, Background } from "@xyflow/react";
+"use client";
+
+import { useState, useCallback } from "react";
+
+import {
+  ReactFlow,
+  Background,
+  applyNodeChanges,
+  applyEdgeChanges,
+  NodeChange,
+  EdgeChange,
+  Node,
+  Edge,
+  Connection,
+  addEdge,
+} from "@xyflow/react";
 import "@xyflow/react/dist/style.css";
 
 import { Controls } from "./controls";
 
-const nodes = [
+const initialNodes: Node[] = [
   {
     id: "1",
     type: "input",
@@ -24,15 +39,42 @@ const nodes = [
   },
 ];
 
-const edges = [
+const initialEdges: Edge[] = [
   { id: "1", source: "1", target: "2" },
   { id: "2", source: "2", target: "3" },
 ];
 
 export function Graph() {
+  const [nodes, setNodes] = useState<Node[]>(initialNodes);
+  const [edges, setEdges] = useState<Edge[]>(initialEdges);
+
+  const onNodesChange = useCallback(
+    (changes: NodeChange[]) =>
+      setNodes((nds) => applyNodeChanges(changes, nds)),
+    [setNodes]
+  );
+
+  const onEdgesChange = useCallback(
+    (changes: EdgeChange[]) =>
+      setEdges((eds) => applyEdgeChanges(changes, eds)),
+    [setEdges]
+  );
+
+  const onConnect = useCallback(
+    (connection: Connection) => setEdges((eds) => addEdge(connection, eds)),
+    [setEdges]
+  );
+
   return (
     <div className="h-full w-full relative">
-      <ReactFlow nodes={nodes} edges={edges} fitView>
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        fitView
+      >
         <Controls />
         <Background />
       </ReactFlow>
