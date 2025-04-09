4acecabcd54d38df5eee95f5d7f94e2b0f929fcd
Alexandros Lekkas <alexandros.lekkas@outlook.com>
2025-04-08 21:09:00 -0500

    feat: Enhance AnalysisNode component to fetch and display graph states with improved UI elements and error handling

--- src/app/dashboard/graphs/[id]/nodes/analysis-node.tsx
+++ src/app/dashboard/graphs/[id]/nodes/analysis-node.tsx
@@ -1,12 +1,70 @@
 "use client";
 
+import { useEffect, useState } from "react";
+
+import { Pencil } from "lucide-react";
 import { Handle, Position } from "@xyflow/react";
+import { toast } from "sonner";
+
+import { createClient } from "@/lib/supabase/client";
+
+import { Tables } from "@/types/supabase";
+
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+
+import { useGraph } from "../layout";
 
 export function AnalysisNode() {
+  const { graphId } = useGraph();
+
+  const [states, setStates] = useState<Tables<"graph_states">[]>([]);
+
+  useEffect(() => {
+    const fetchStates = async () => {
+      const supabase = createClient();
+
+      const { data, error } = await supabase
+        .from("graph_states")
+        .select("*")
+        .eq("graph_id", graphId);
+
+      if (error) {
+        console.error(error);
+        toast.error("Failed to fetch states");
+        return;
+      }
+
+      setStates(data ?? []);
+    };
+    fetchStates();
+  }, [graphId]);
+
   return (
     <>
-      <div className="rounded-full bg-gray-200 p-2">
-        <span>I'm analyzing!</span>
+      <span className="text-sm text-foreground/50 text-center font-medium block mb-1">
+        Analysis
+      </span>
+
+      <div className="rounded-2xl bg-muted border-2 backdrop-blur-md p-4 space-y-3">
+        {states.map((state) => (
+          <div key={state.id} className="flex items-center gap-3">
+            <div className="flex flex-1">
+              <Badge variant="default" className="text-md py-1.5 px-6">
+                {state.name.charAt(0).toUpperCase() + state.name.slice(1)}
+              </Badge>
+            </div>
+
+            <Badge variant="outline" className="text-md py-1.5 px-6">
+              {state.type.charAt(0).toUpperCase() +
+                state.type.slice(1).toLowerCase()}
+            </Badge>
+
+            <Button variant="outline" size="icon">
+              <Pencil className="size-4" />
+            </Button>
+          </div>
+        ))}
       </div>
 
       <Handle
